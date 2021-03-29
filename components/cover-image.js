646d35da6d84d471841526f279844efc0a761daa
kilesh11 <kyle.suen11@gmail.com>
2021-03-29 18:48:15 +0800

    update styling, add prism for md code block syntax highlighting

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,20 +1,20 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-export default function CoverImage({ title, url, slug }) {
-  const imageUrl = `${
-    url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''
-  }${url}`
-  return (
-    <div className="sm:mx-0">
-      {slug ? (
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a aria-label={title}>
-            <img src={imageUrl} alt={title} />
-          </a>
-        </Link>
-      ) : (
-        <img src={imageUrl} alt={title} />
-      )}
-    </div>
-  )
+export default function CoverImage({title, url, slug}) {
+    const imageUrl = `${
+        url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''
+    }${url}`;
+    return (
+        <div className="sm:mx-0">
+            {slug ? (
+                <Link as={`/posts/${slug}`} href="/posts/[slug]">
+                    <a aria-label={title}>
+                        <img className="mx-auto" src={imageUrl} alt={title} />
+                    </a>
+                </Link>
+            ) : (
+                <img className="mx-auto" src={imageUrl} alt={title} />
+            )}
+        </div>
+    );
 }
--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,25 @@
-import Alert from './alert'
-import Footer from './footer'
-import Meta from './meta'
+import {useEffect} from 'react';
+import Alert from './alert';
+import Footer from './footer';
+import Meta from './meta';
+import Prism from 'prismjs';
+import '@/styles/prism';
 
-export default function Layout({ preview, children }) {
-  return (
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
-  )
+export default function Layout({preview, children}) {
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            Prism.highlightAll();
+        }
+    }, []);
+
+    return (
+        <>
+            <Meta />
+            <div className="min-h-screen">
+                <Alert preview={preview} />
+                <main>{children}</main>
+            </div>
+            <Footer />
+        </>
+    );
 }
