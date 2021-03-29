03ad826c25b6b1abbe7ced3292e4f362a4cb8247
kilesh11 <kyle.suen11@gmail.com>
2021-03-30 01:18:44 +0800

    optimize prism, small tweak on UI

--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,8 @@
-import {useEffect} from 'react';
 import Alert from './alert';
 import Footer from './footer';
 import Meta from './meta';
-import Prism from 'prismjs';
-import '@/styles/prism';
 
 export default function Layout({preview, children}) {
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            Prism.highlightAll();
-        }
-    }, []);
-
     return (
         <>
             <Meta />
