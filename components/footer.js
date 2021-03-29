10978c2bef3bef9a7b0ae683c74b0287d0a3b91b
kilesh11 <kyle.suen11@gmail.com>
2021-03-30 01:58:45 +0800

    UI adjustment

--- components/footer.js
+++ components/footer.js
@@ -8,17 +8,15 @@ export default function Footer() {
             <Container>
                 <div className="py-28 flex flex-col lg:flex-row items-center">
                     <div className="mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-                        <span className="pr-4">
-                            <a href="https://nextjs.org/">
-                                <Image
-                                    src="/next-js.svg"
-                                    alt="Picture of the author"
-                                    width={40}
-                                    height={40}
-                                />
-                            </a>
-                        </span>
-                        <a href="https://strapi.io/">
+                        <a className="pr-4" href="https://nextjs.org/">
+                            <Image
+                                src="/next-js.svg"
+                                alt="Picture of the author"
+                                width={40}
+                                height={40}
+                            />
+                        </a>
+                        <a className="pr-4" href="https://strapi.io/">
                             <Image
                                 src="/strapi.svg"
                                 alt="Picture of the author"
@@ -29,17 +27,18 @@ export default function Footer() {
                     </div>
 
                     <div className="flex flex-col lg:flex-row lg:justify-end justify-center items-center lg:pl-4 lg:w-1/2">
+                        <h4 className="mx-3 ">Modified by</h4>
+                        <h3 className="mx-3 font-bold">Kyle Suen</h3>
                         <a
-                            href="https://nextjs.org/docs/basic-features/pages"
-                            className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+                            className="mt-4 lg:mt-0"
+                            href="https://github.com/kilesh11"
                         >
-                            Read Documentation
-                        </a>
-                        <a
-                            href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                            className="mx-3 font-bold hover:underline"
-                        >
-                            View on GitHub
+                            <Image
+                                src="/github.svg"
+                                alt="Picture of the author"
+                                width={40}
+                                height={40}
+                            />
                         </a>
                     </div>
                 </div>
