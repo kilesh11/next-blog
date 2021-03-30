93d1547d721af3bf78634bd326fe0e12ae22a75e
kilesh11 <kyle.suen11@gmail.com>
2021-03-31 03:35:47 +0800

    upgrade img tag to next image for better loading performance

--- components/avatar.js
+++ components/avatar.js
@@ -1,13 +1,21 @@
+import Image from 'next/image';
+
 export default function Avatar({ name, picture }) {
     const url = picture.url ?? picture[0].url;
 
     return (
         <div className="flex items-center">
-            <img
-                src={`${url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${url}`}
-                className="w-12 h-12 rounded-full mr-4 grayscale"
-                alt={name}
-            />
+            <div className="w-12 h-12  mr-4">
+                <Image
+                    src={`${
+                        url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''
+                    }${url}`}
+                    alt={name}
+                    width={48}
+                    height={48}
+                    className="rounded-full"
+                />
+            </div>
             <div className="text-xl font-bold">{name}</div>
         </div>
     );
--- components/cover-image.js
+++ components/cover-image.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function CoverImage({ title, url, slug }) {
     const imageUrl = `${url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${url}`;
@@ -7,11 +8,17 @@ export default function CoverImage({ title, url, slug }) {
             {slug ? (
                 <Link as={`/posts/${slug}`} href="/posts/[slug]">
                     <a aria-label={title}>
-                        <img className="mx-auto" src={imageUrl} alt={title} />
+                        <Image
+                            src={imageUrl}
+                            alt={title}
+                            width={1600}
+                            height={900}
+                            className="mx-auto"
+                        />
                     </a>
                 </Link>
             ) : (
-                <img className="mx-auto" src={imageUrl} alt={title} />
+                <Image className="mx-auto" src={imageUrl} alt={title} width={1600} height={900} />
             )}
         </div>
     );
