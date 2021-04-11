a1126d04d307e6327d5a5527bb1b74ed126587ca
kilesh11 <kyle.suen11@gmail.com>
2021-04-12 01:43:09 +0800

    add default row

--- components/more-stories.js
+++ components/more-stories.js
@@ -14,7 +14,7 @@ export default function MoreStories({ posts, postsPerPage = null }) {
             <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
                 More Stories
             </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-16">
+            <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-16 md:grid-rows-2">
                 {slicedPosts.map((post) => (
                     <PostPreview
                         key={post.slug}
