efbc3ae01445edaee33d8c36b3d3a35e6bf9afce
kilesh11 <kyle.suen11@gmail.com>
2021-04-12 01:49:32 +0800

    adjust intro size

--- components/intro.js
+++ components/intro.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Intro() {
     return (
         <section className="flex-row flex items-baseline justify-between mt-12 mb-12">
-            <h1 className="text-7xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+            <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
                 Blog.
             </h1>
             <h1 className="text-3xl md:text-4xl tracking-tighter leading-tight md:pl-8">
